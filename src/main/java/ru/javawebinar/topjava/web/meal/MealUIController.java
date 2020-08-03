1a2d1f6cf6ec4d3c24924760d6ee00e2b03950ed
Spayn <aukin2006@ya.ru>
2020-08-03 19:54:59 +0300

    task done
    3: Перевести работу фильтра на AJAX. Попробуйте после модификации таблицы (например добавлении записи) обновлять ее также с учетом фильтра.

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -8,7 +8,9 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
@@ -43,5 +45,26 @@ public class MealUIController extends AbstractMealController {
             meal.setId(id);
             super.update(meal, SecurityUtil.authUserId());
         }
+    }
+
+//    @GetMapping("/filter")
+//    public List<MealTo> filter(
+//            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+//            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+//            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime startTime,
+//            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime endTime
+//    ) {
+//        return super.getBetween(startDate, startTime, endDate, endTime);
+//    }
+
+    @Override
+    @GetMapping("/filter")
+    public List<MealTo> getBetween(
+            @RequestParam LocalDate startDate,
+            @RequestParam LocalTime startTime,
+            @RequestParam LocalDate endDate,
+            @RequestParam LocalTime endTime) {
+        List<MealTo> list = super.getBetween(startDate, startTime, endDate, endTime);
+        return list;
     }
 }
